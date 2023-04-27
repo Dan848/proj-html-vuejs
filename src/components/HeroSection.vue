d50ac1485b50f17a045749d9c08e7442504947f3
Dan848 <daniele.ceravolo15@gmail.com>
2023-04-27 16:48:53 +0200

    complete background hero section

--- src/components/HeroSection.vue
+++ src/components/HeroSection.vue
@@ -1,8 +1,35 @@
 <template>
+    <!-- HERO -->
     <div class="hero">
+        <!-- Hero Background -->
+        <div class="hero-bg left-man">
+            <img src="/imgs/left-man.png" alt="left-man">
+        </div>
+        <div class="hero-bg right-man">
+            <img src="/imgs/right-man.png" alt="right-man">
+        </div>
+        <div class="hero-bg left-dots">
+            <img src="/imgs/dots-left.png" alt="dots-left">
+        </div>
+        <div class="hero-bg right-dots">
+            <img src="/imgs/dots-right.png" alt="dots-right">
+        </div>
+        <div class="hero-bg globe-1">
+            <img src="/imgs/globe1.png" alt="globe1">
+        </div>
+        <div class="hero-bg globe-2">
+            <img src="/imgs/globe2.png" alt="globe2">
+        </div>
+        <div class="hero-bg globe-3">
+            <img src="/imgs/globe3.png" alt="globe3">
+        </div>
+        <div class="hero-bg light">
+            <img src="/imgs/light.png" alt="light">
+        </div>
+        <!-- Hero Container -->
         <div class="container">
             <div class="row justify-content-center">
-                <div class="col-xl-10 text-center">
+                <div class="col-xxl-12 text-center">
                     <h5 class="mb-3">{{ obj.subTitle }}</h5>
                     <h1>We Animate</h1>
                     <h1 class="mb-2">{{ obj.title }}</h1>
@@ -18,6 +45,7 @@
             </div>
         </div>
     </div>
+    <!-- /HERO -->
 </template>
 
 <script>
@@ -36,26 +64,107 @@
 <style lang="scss" scoped>
 @use "../assets/styles/partials/variables" as *;
     .hero {
+
         padding-top: 150px;
         background: url("./imgs/banner-bg.png");
         background-size: cover;
         position: relative;
-        h5 {
+
+        //Make background a little bit darker for visibility
+        &::before{
+            content: "";
+            display: block;
+            position: absolute;
+            width: 100%;
+            height: 100%;
+            top: 0;
+            left: 0;
+            background-color: rgba(0, 0, 0, 0.1);
+        }
+        .hero-bg {
+            position: absolute;
+            &.left-man {
+                z-index: 60;
+                top: 25%;
+                left: 2%;
+                max-width: 280px;
+            }
+            &.right-man {
+                z-index: 60;
+                top: 25%;
+                right: 2%;
+                max-width: 280px;
+            }
+            &.left-dots {
+                z-index: 10;
+                top: 10%;
+                left: 0;
+                right: 50%;
+            }
+            &.right-dots {
+                z-index: 10;
+                top: 10%;
+                right: 0;
+                left: 50%;
+            }
+            &.globe-1 {
+                z-index: 40;
+                top: 10%;
+                left: 10%;
+                max-width: 110px;
+            }
+            &.globe-2 {
+                z-index: 20;
+                right: 30%;
+                max-width: 95px;
+            }
+            &.globe-3 {
+                z-index: 30;
+                top: -20%;
+                left: 40%;
+                max-width: 250px;
+            }
+            &.light {
+                z-index: 50;
+                top: 10%;
+                right: 5%;
+                max-width: 110px;
+            }
+            img{
+                width: 100%;
+                height: 100%;
+            }
+        }
+
+        .container{
+            position: relative;
+            z-index: 100;
+            h5 {
             color: $dc-secondary-two;
             font-weight: 600;
+            }
+            h1 {
+                color: $dc-white;
+                font-weight: 700;
+                font-size: 4rem;
+            }
+            p {
+                color: $dc-white;
+                font-size: 1.2rem;
+            }
+            .jumbotron {
+                transform: translateY(170px);
+                margin-top: -170px;
+            }
         }
-        h1 {
-            color: $dc-white;
-            font-weight: 700;
-            font-size: 4rem;
-        }
-        p {
-            color: $dc-white;
-            font-size: 1.2rem;
-        }
-        .jumbotron {
-            transform: translateY(170px);
-            margin-top: -170px;
+    }
+
+    @media screen and (max-width: 1400px) {
+        .left-man {
+        max-width: 200px!important;
+            }
+        .right-man {
+        max-width: 200px!important;
         }
     }
 </style>
